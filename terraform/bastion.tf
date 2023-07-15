2c9835cbd845d454240b76fdffedfa04f5ff8e58
Andrei Sotnikov <sotnikov.andrey07@gmail.com>
2023-07-15 20:39:28 +0300

    Setup bastion host

---
+++ terraform/bastion.tf
@@ -0,0 +1,53 @@
+resource "yandex_compute_instance" "bh" {
+  name = "bastion-host"
+
+  zone = yandex_vpc_subnet.subnet-b.zone
+
+  resources {
+    cores = 2
+    core_fraction = 20
+    memory = 1
+    
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = local.image_id
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-b.id
+    nat       = true
+    dns_record {
+      fqdn = "bastion.dip.lom."
+    }
+  }
+
+  metadata = {
+    user-data = "${file("./bastion.yml")}"
+  }
+
+#  provisioner "file" {
+#    source = "./ansible_controller/ansible.key"
+#    destination = "/home/ansible/.ssh/ansible.key"
+#  }
+
+}
+
+resource "yandex_compute_snapshot_schedule" "bh-snapshot" {
+  name = "bastion-snapshot"
+
+  schedule_policy {
+    expression = "45 21 * * *" # Every day at 21:45
+  }
+
+  snapshot_count = 7  # Maximum snapshots
+
+  snapshot_spec {
+    labels = { target = "bastion"}
+  }
+
+  disk_ids = [ yandex_compute_instance.bh.boot_disk[0].disk_id ]
+
+}
