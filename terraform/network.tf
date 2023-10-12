69ca9e16ad104d29db34ef5a431071214010b8c7
Andrei Sotnikov <sotnikov.andrey07@gmail.com>
2023-10-13 00:17:22 +0300

    Add security groups

--- terraform/network.tf
+++ terraform/network.tf
@@ -39,3 +39,125 @@ resource "yandex_vpc_route_table" "nat-instance-route" {
     gateway_id         = yandex_vpc_gateway.nat_gateway.id
   }
 }
+
+resource "yandex_vpc_security_group" "default-sg" {
+  name        = "default-sg"
+  description = "Default security group"
+  network_id = yandex_vpc_network.network-1.id
+
+  ingress {
+    description       = "Allow ICMP"
+    protocol          = "ICMP"
+    port              = 443
+  }
+
+  egress {
+    description       = "Permit ANY"
+    protocol          = "ANY"
+    v4_cidr_blocks    = ["0.0.0.0/0"]
+  }
+}
+
+resource "yandex_vpc_security_group" "web-sg" {
+  name        = "web-sg"
+  description = "Security group for web servers"
+  network_id = yandex_vpc_network.network-1.id
+
+  ingress {
+    description       = "Allow HTTPS"
+    protocol          = "TCP"
+    port              = 443
+  }
+
+  ingress {
+    description       = "Allow HTTP"
+    protocol          = "TCP"
+    port              = 80
+  }
+}
+
+resource "yandex_vpc_security_group" "internal-ssh-sg" {
+  name        = "internal-ssh-sg"
+  network_id = yandex_vpc_network.network-1.id
+
+  ingress {
+    description       = "Allow SSH"
+    protocol          = "TCP"
+    port              = 21
+    v4_cidr_blocks = [yandex_vpc_subnet.public-subnet.v4_cidr_blocks[*],
+                      yandex_vpc_subnet.private-subnet-a.v4_cidr_blocks[*],
+                      yandex_vpc_subnet.private-subnet-b.v4_cidr_blocks[*]]
+  }
+}
+
+resource "yandex_vpc_security_group" "external-ssh-sg" {
+  name        = "external-ssh-sg"
+  network_id = yandex_vpc_network.network-1.id
+
+  ingress {
+    description       = "Allow SSH"
+    protocol          = "TCP"
+    port              = 21
+  }
+}
+
+resource "yandex_vpc_security_group" "kibana-sg" {
+  name        = "kibana-sg"
+  network_id = yandex_vpc_network.network-1.id
+
+  ingress {
+    description       = "Allow 5601"
+    protocol          = "TCP"
+    port              = 5601
+  }
+}
+resource "yandex_vpc_security_group" "es-sg" {
+  name        = "es-sg"
+  network_id = yandex_vpc_network.network-1.id
+
+  ingress {
+    description       = "Allow 9200"
+    protocol          = "TCP"
+    port              = 9200
+  }
+}
+
+resource "yandex_vpc_security_group" "grafana-sg" {
+  name        = "grafana-sg"
+  network_id = yandex_vpc_network.network-1.id
+
+  ingress {
+    description       = "Allow 3000"
+    protocol          = "TCP"
+    port              = 3000
+  }
+}
+
+resource "yandex_vpc_security_group" "prometheus-sg" {
+  name        = "prometheus-sg"
+  network_id = yandex_vpc_network.network-1.id
+
+  ingress {
+    description       = "Allow 9090"
+    protocol          = "TCP"
+    port              = 9090
+  }
+}
+
+resource "yandex_vpc_security_group" "metrics-sg" {
+  name        = "metrics-sg"
+  network_id = yandex_vpc_network.network-1.id
+
+  ingress {
+    description       = "Allow nginxlog_exporter"
+    protocol          = "TCP"
+    port              = 4040
+  }
+
+    ingress {
+    description       = "Allow node_exporter"
+    protocol          = "TCP"
+    port              = 9100
+  }
+
+}
